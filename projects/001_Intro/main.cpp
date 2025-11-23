41e66f63698e1c588c1213ec2010ddf4697db643
Pan Thistle <60625336+panthistle@users.noreply.github.com>
2025-11-23 23:07:41 +0200

    Add files via upload

---
+++ projects/001_Intro/main.cpp
@@ -0,0 +1,12 @@
+// c++ example: Intro
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Kind World!" << std::endl;
+    std::cout << "Goodbye Cruel World! \n";
+
+    // return normally, 0 means no error
+    return 0;
+}
